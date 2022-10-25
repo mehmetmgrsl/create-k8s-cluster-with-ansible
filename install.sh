ca509f61f5b73410cc52c6e2ca697ceab91664bf
mehmetmgrsl <mehmetgursul@gmail.com>
2022-10-25 12:06:55 +0200

    ping commands have been added

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ vagrant up
 cd ..
 
 cd  kubeadm-ansible
+ansible -i hosts all -m ping
 sudo ansible-playbook -i hosts node-init.yml
+ansible -i hosts all -m ping
 sudo ansible-playbook -i hosts master-node-init.yml
+ansible -i hosts all -m ping
 sudo ansible-playbook -i hosts worker-node-init.yml
+ansible -i hosts all -m ping
